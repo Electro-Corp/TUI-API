8d4fc580f8c1930e5a20f70837553a598a1488d9
the_electro_bros <chinmaytanmay@outlook.com>
2022-11-08 21:39:43 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "tui.h"
+int main(void) {
+  struct menu mainMen = {"Main Menu","Read","Write","Quit"};
+  int re = createMenu(mainMen);
+  if(re == 2)
+    exit(0);
+  else if(re == 0){
+    char* fileName = inputBox("File Name:");
+    FILE* fp = fopen(fileName,"r");
+    if(!fp){
+      printf("File %s does not exist!\n",fileName);
+      exit(1);
+    }
+    char buffer[10000];
+    fread(buffer,sizeof(buffer),1,fp);
+    //printf("%s\n",buffer);
+    alert("File",buffer);
+  }
+  else if(re == 1){
+    char* fileName = inputBox("File Name:");
+    FILE* fp = fopen(fileName,"w");
+    if(!fp){
+      printf("File %s does not exist!\n",fileName);
+      exit(1);
+    }
+    char* toAdd = inputBox("Text to append: ");
+    fwrite(toAdd,1, sizeof(toAdd)*5,fp);
+  }
+  return 0;
+}
---
+++ tui.h
@@ -0,0 +1,111 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+struct menu {
+  char *title;
+  char *option0;
+  char *option1;
+  char *option2;
+};
+int createMenu(struct menu m) {
+  struct termios info;
+  tcgetattr(0, &info);
+  info.c_lflag &= ~ICANON;
+  info.c_cc[VMIN] = 1;
+  info.c_cc[VTIME] = 0;
+  tcsetattr(0, TCSANOW, &info);
+  int option = 0;
+  char c;
+  while (1) {
+    c = NULL;
+    system("clear");
+    while (c == NULL) {
+      printf("[%s]\n", m.title);
+      switch (option) {
+      case 0:
+        printf("[==%s==]\n", m.option0);
+        printf("--%s--\n", m.option1);
+        printf("--%s--\n", m.option2);
+        break;
+      case 1:
+        printf("--%s--\n", m.option0);
+        printf("[==%s==]\n", m.option1);
+        printf("--%s--\n", m.option2);
+        break;
+      case 2:
+        printf("--%s--\n", m.option0);
+        printf("--%s--\n", m.option1);
+        printf("[==%s==]\n", m.option2);
+        break;
+      }
+      c = getchar();
+    }
+    if (c == 'w' && option != 0) {
+      option--;
+    }
+    if (c == 's' && option != 2) {
+      option++;
+    }
+    if (c == '\n') {
+      break;
+    }
+  }
+  system("clear");
+  return option;
+}
+char *inputBox(char *dialog) {
+  char *input = malloc(100);
+  int index = 0;
+  char c;
+  while (c != '\n') {
+    c = NULL;
+    system("clear");
+    if (strlen(dialog) > strlen(input)) {
+      for (int i = 0; i < strlen(dialog) / 2; i++) {
+        printf("=");
+      }
+      printf("%s", dialog);
+      for (int i = 0; i < strlen(dialog) / 2; i++) {
+        printf("=");
+      }
+    } else {
+      for (int i = 0; i < strlen(input) / 2; i++) {
+        printf("=");
+      }
+      printf("%s", dialog);
+      for (int i = 0; i < strlen(input) / 2; i++) {
+        printf("=");
+      }
+    }
+    printf("\n");
+    for (int i = 0; i < strlen(input) / 2; i++) {
+      printf("-");
+    }
+    printf("%s", input);
+    for (int i = 0; i < strlen(input) / 2; i++) {
+      printf("-");
+    }
+    c = getchar();
+    if (c != 8 && c != 10) {
+      input[index++] = c;
+    } else {
+      input[index--] = '\0';
+      index--;
+    }
+  }
+  system("clear");
+  return input;
+}
+void alert(char *title, char *body) {
+  system("clear");
+  for (int i = 0; i < strlen(title) / 2; i++) {
+    printf("=");
+  }
+  printf(title);
+  for (int i = 0; i < strlen(title) / 2; i++) {
+    printf("=");
+  }
+  printf("\n");
+  
+}
